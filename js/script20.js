604547d35941be769820cbe8ef566b89d6404c00
Nicolas Miranda <nicolascpcarvalho@gmail.com>
2025-10-04 16:00:17 -0300

    Update script20.js

--- js/script20.js
+++ js/script20.js
@@ -2,5 +2,5 @@ let valor1 = parseFloat(prompt("Digite o primeiro valor: "))
 let valor2 = parseFloat(prompt("Digite o primeiro valor: "))
 
 if( valor1 > valor2){
-    alert("A odem crescnte é: " + valor2 + " ," + valor1)
+    alert("A ordem crescente é: " + valor2 + " ," + valor1)
 }
